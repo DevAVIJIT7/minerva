63ac336594d2e485d21def029b160b0beebc2658
Evgeniy <evgeniyp@claned.com>
2019-03-07 11:12:36 +0300

    Add minerva search to rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,86 @@
 module RailsAdmin
   module Config
     module Actions
+      class Index < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+        register_instance_option :controller do
+          proc do
+            if params[:model_name] == 'minerva~resource' && params[:minerva_query].present?
+              result = Minerva::Search::Engine.new(
+                  {filter: "search='#{params[:minerva_query]}'",
+                   limit: RailsAdmin::Config.default_items_per_page,
+                   offset: (params.fetch(:page, 1).to_i-1) * RailsAdmin::Config.default_items_per_page}, nil)
+                           .perform
+
+              current_page = params.fetch(:page, 1).to_i
+              total_pages = (result.pagination[:total_count] / (1.0*RailsAdmin::Config.default_items_per_page)).ceil
+
+              @objects = Minerva::Resource.where(id: result.resources.map(&:id))
+              @objects.define_singleton_method(:total_pages) { total_pages }
+              @objects.define_singleton_method(:total_count) { result.pagination[:total_count] }
+              @objects.define_singleton_method(:current_page) { current_page }
+            else
+              @objects = list_entries
+            end
+
+            unless @model_config.list.scopes.empty?
+              if params[:scope].blank?
+                unless @model_config.list.scopes.first.nil?
+                  @objects = @objects.send(@model_config.list.scopes.first)
+                end
+              elsif @model_config.list.scopes.collect(&:to_s).include?(params[:scope])
+                @objects = @objects.send(params[:scope].to_sym)
+              end
+            end
+
+            respond_to do |format|
+              format.html do
+                render @action.template_name, status: @status_code || :ok
+              end
+
+              format.json do
+                output = begin
+                  if params[:compact]
+                    primary_key_method = @association ? @association.associated_primary_key : @model_config.abstract_model.primary_key
+                    label_method = @model_config.object_label_method
+                    @objects.collect { |o| {id: o.send(primary_key_method).to_s, label: o.send(label_method).to_s} }
+                  else
+                    @objects.to_json(@schema)
+                  end
+                end
+                if params[:send_data]
+                  send_data output, filename: "#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.json"
+                else
+                  render json: output, root: false
+                end
+              end
+
+              format.xml do
+                output = @objects.to_xml(@schema)
+                if params[:send_data]
+                  send_data output, filename: "#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.xml"
+                else
+                  render xml: output
+                end
+              end
+
+              format.csv do
+                header, encoding, output = CSVConverter.new(@objects, @schema).to_csv(params[:csv_options].permit!.to_h)
+                if params[:send_data]
+                  send_data output,
+                            type: "text/csv; charset=#{encoding}; #{'header=present' if header}",
+                            disposition: "attachment; filename=#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.csv"
+                elsif Rails.version.to_s >= '5'
+                  render plain: output
+                else
+                  render text: output
+                end
+              end
+            end
+          end
+        end
+      end
+
       class Import < RailsAdmin::Config::Actions::Base
         RailsAdmin::Config::Actions.register(self)
         register_instance_option :collection do
